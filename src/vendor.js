aee6824de14c1f0369aa8d9904155238fd78e34b
Shyam Chen <shyamchen1994@gmail.com>
2016-11-10 13:56:13 +0800

    58th Commit

--- src/vendor.js
+++ src/vendor.js
@@ -1,15 +1,15 @@
 /*
  * Polyfills
  */
-import 'core-js/client/shim.min.js';
+// import 'core-js/client/shim.min.js';
 
 /*
  * Libraries
  */
 // CSS
-import 'material-design-lite/dist/material.deep_purple-indigo.min.css';
+// import 'material-design-lite/dist/material.deep_purple-indigo.min.css';
 
 // JS
-import 'material-design-lite/dist/material.min.js';
-import 'firebase/firebase.js';
-import 'page/page.js';
+// import 'material-design-lite/dist/material.min.js';
+// import 'firebase/firebase.js';
+// import 'page/page.js';
