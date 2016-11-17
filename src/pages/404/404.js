97ac7dd3803bf0f3d4035058b31197d4b96dae59
Shyam Chen <shyamchen1994@gmail.com>
2016-11-17 14:26:45 +0800

    104th Commit

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,7 +1,18 @@
-import notFound from './404.html';
+import template from 'lodash-es/template';
+
+import notFoundTpl from './404.html';
+import notFoundStyle from './404.css';
 
 import { query } from '../../components/utils';
 
+export const notFoundOpts = {
+  text: '404',
+  style: {
+    page: notFoundStyle.page,
+    text: notFoundStyle.text
+  }
+};
+
 export const load404 = () => {
-  query('#app').innerHTML = notFound;
+  query('#app').innerHTML = template(notFoundTpl)(notFoundOpts);
 };
