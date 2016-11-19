1d2d632872048f80909e0d3ec0e92686823531a6
Shyam Chen <shyamchen1994@gmail.com>
2016-11-19 14:40:02 +0800

    122nd Commit

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -2,6 +2,7 @@ import template from 'lodash-es/template';
 
 import { query } from '../../components/utils';
 import { layout } from '../../components/layout';
+import colorData from '../../assets/datas/color-bar-chart.json';
 
 import about from './about.html';
 
@@ -9,43 +10,21 @@ export const loadAbout = () => {
   query('#app').innerHTML = layout;
   query('#page').innerHTML = template(about)({ });
 
-
-  const ctx = document.getElementById("myChart");
-  const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
+  new Chart(query('#color-bar-chart'), {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [colorData]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
           }
+        }]
       }
+    }
   });
 
 
