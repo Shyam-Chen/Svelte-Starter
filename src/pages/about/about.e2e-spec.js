365a761af886cbbc99c7352ab8a4a5e4a6a46695
Shyam Chen <shyamchen1994@gmail.com>
2016-11-19 17:15:34 +0800

    126th Commit

--- src/pages/about/about.e2e-spec.js
+++ src/pages/about/about.e2e-spec.js
@@ -4,9 +4,13 @@ describe('About', () => {
     webdriver.get('http://localhost:9876/about');
   });
 
-  it('getText toEqual', () => {
-    let el = webdriver.findElement(by.css('#page p:nth-child(1)'));
-    expect(el.getText()).toEqual('About');
+  it('getCurrentUrl toMatch', () => {
+    expect(webdriver.getCurrentUrl()).toMatch('/about');
   });
+
+  // it('getText toEqual', () => {
+  //   let el = webdriver.findElement(by.css('#page p:nth-child(1)'));
+  //   expect(el.getText()).toEqual('About');
+  // });
 
 });
--- src/pages/home/home.e2e-spec.js
+++ src/pages/home/home.e2e-spec.js
@@ -4,9 +4,13 @@ describe('Home', () => {
     webdriver.get('http://localhost:9876/');
   });
 
-  it('getText toEqual', () => {
-    let el = webdriver.findElement(by.css('#page p:nth-child(1)'));
-    expect(el.getText()).toEqual('Home');
+  it('getCurrentUrl toMatch', () => {
+    expect(webdriver.getCurrentUrl()).toMatch('/');
   });
+
+  // it('getText toEqual', () => {
+  //   let el = webdriver.findElement(by.css('#page p:nth-child(1)'));
+  //   expect(el.getText()).toEqual('Home');
+  // });
 
 });
