1082c3087062cd84132c987f48a3f6bb3f99a352
Shyam Chen <shyamchen1994@gmail.com>
2016-11-26 18:01:08 +0800

    176th Commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -ex
+
 docker-compose exec app yarn run prod
 docker-compose exec app npm run deploy -- --token ${FIREBASE_TOKEN}
---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+if [[ "$MODE" == "e2e" ]]; then
+  
+fi
