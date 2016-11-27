7a43f1c2c370ad519ae979973ef1e4ba94e5c7be
Shyam Chen <shyamchen1994@gmail.com>
2016-11-27 20:40:57 +0800

    189th Commit

--- browserstack.conf.js
+++ browserstack.conf.js
@@ -1,6 +1,5 @@
 const babel = require('babel-core/register');
 
-// OK
 exports.config = {
   specs: ['./src/**/*.e2e-spec.js'],
   exclude: [],
@@ -16,45 +15,39 @@ exports.config = {
     includeStackTrace: false
   },
   seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
-  // commonCapabilities: {
-  capabilities: {
+  commonCapabilities: {
     'browserstack.user': process.env.BROWSERSTACK_USERNAME,
     'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'E2E Tests',
     'browserstack.local': true,
-    'browserstack.debug': 'true',
-    'os': 'Windows',
-    'os_version': '10',
-    'browserName': 'Chrome',
-    'browser_version': '54.0',
-    'resolution': '1280x1024'
-  }// ,
-  // multiCapabilities: [
-  //   {
-  //     'os': 'Windows',
-  //     'os_version': '10',
-  //     'browserName': 'Edge',
-  //     'browser_version': '13.0',
-  //     'resolution': '1280x1024'
-  //   }, {
-  //     'os': 'OS X',
-  //     'os_version': 'Yosemite',
-  //     'browserName': 'Safari',
-  //     'browser_version': '8.0',
-  //     'resolution': '1280x1024'
-  //   }, {
-  //     'browserName': 'android',
-  //     'platform': 'ANDROID',
-  //     'device': 'Google Nexus 5'
-  //   }, {
-  //     'browserName': 'iPhone',
-  //     'platform': 'MAC',
-  //     'device': 'iPhone 6'
-  //   }
-  // ]
+    'browserstack.debug': 'true'
+  },
+  multiCapabilities: [
+    {
+      'os': 'Windows',
+      'os_version': '10',
+      'browserName': 'Edge',
+      'browser_version': '14.0',
+      'resolution': '1280x1024'
+    }, {
+      'os': 'OS X',
+      'os_version': 'Sierra',
+      'browserName': 'Safari',
+      'browser_version': '10.0',
+      'resolution': '1280x1024'
+    }, {
+      'browserName': 'android',
+      'platform': 'ANDROID',
+      'device': 'Google Nexus 5'
+    }, {
+      'browserName': 'iPhone',
+      'platform': 'MAC',
+      'device': 'iPhone 6S Plus'
+    }
+  ]
 };
 
-// exports.config.multiCapabilities.forEach((caps) => {
-//   for(let i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
-// });
+exports.config.multiCapabilities.forEach((caps) => {
+  for(let i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
+});
