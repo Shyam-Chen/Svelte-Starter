133c5612829c56b3d0d87b4c4852b85161cd8b78
Shyam Chen <shyamchen1994@gmail.com>
2016-11-28 17:54:53 +0800

    204th Commit

--- src/pages/home/home.e2e-spec.js
+++ src/pages/home/home.e2e-spec.js
@@ -4,6 +4,10 @@ describe('Home', () => {
     webdriver.get('http://localhost:9876/en/home');
   });
 
+  it('getCurrentUrl toEqual', () => {
+    expect(webdriver.getCurrentUrl()).toEqual('http://localhost:9876/en/home');
+  });
+
   it('getText toEqual', () => {
     let el = webdriver.findElement(by.css('#page p:nth-child(1)'));
     expect(el.getText()).toEqual('Home');
