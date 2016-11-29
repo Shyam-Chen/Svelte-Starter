e27975bffa1135d7c8fd8d4ba82eeb33a0ec234f
Shyam Chen <shyamchen1994@gmail.com>
2016-11-29 11:46:19 +0800

    208th Commit

--- src/pages/404/404.e2e-spec.js
+++ src/pages/404/404.e2e-spec.js
@@ -1,12 +1,12 @@
 describe('404', () => {
 
-  it('getText toEqual - 1', () => {
+  it('should be 404 page - 1', () => {
     webdriver.get('http://localhost:9876/foo');
     let el = webdriver.findElement(by.css('#app p:nth-child(1)'));
     expect(el.getText()).toEqual('404');
   });
 
-  it('getText toEqual - 2', () => {
+  it('should be 404 page - 2', () => {
     webdriver.get('http://localhost:9876/bar');
     let el = webdriver.findElement(by.css('#app p:nth-child(1)'));
     expect(el.getText()).toEqual('404');
