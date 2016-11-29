13de0849356745c5c6f8dcfb6403b454beb3250e
Shyam Chen <shyamchen1994@gmail.com>
2016-11-29 11:41:55 +0800

    207th Commit

--- src/utils/loade.spec.js
+++ src/utils/loade.spec.js
@@ -6,8 +6,12 @@ describe('Load', () => {
     expect(load).not.toThrow();
   });
 
-  it('toBeDefined', () => {
+  it('should be able to get json', () => {
     expect(load('../assets/datas/firebase.config.json')).toBeDefined();
+  });
+
+  it('should be able to get font', () => {
+    expect(load('https://fonts.googleapis.com/css?family=Lora')).toBeDefined();
   });
 
 });
