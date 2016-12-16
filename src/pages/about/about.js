aaf0caff158a28e3901031e34c8b041a25a706c0
Shyam Chen <shyamchen1994@gmail.com>
2016-12-16 19:11:36 +0800

    304th Commit

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -13,33 +13,30 @@ import LANGS_ZH from './langs/zh.json';
 const imports = { 'imports': { style } };
 
 const chart = () => {
-  new Chart('myChart', {
+  new Chart('pagespeed-insights', {
     type: 'bar',
     data: {
-      labels: ["JS", "Lodash", "ReactiveX", "Material", "Firebase"],
+      labels: ["Mobile", "Desktop"],
       datasets: [
         {
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2],
+          label: 'Score',
+          data: [89, 95],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)'
+            'rgba(111, 99, 255, .2)',
+            'rgba(111, 99, 255, .2)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)'
+            'rgba(111, 99, 255, 1)',
+            'rgba(111, 99, 255, 1)'
           ],
           borderWidth: 1
         }
       ]
     },
-    options: { scales: { yAxes: [{ ticks: { beginAtZero:true } }] } }
+    options: {
+      title: { display: true, text: 'PageSpeed Insights' },
+      scales: { yAxes: [{ ticks: { beginAtZero:true } }] }
+    }
   });
 };
 
