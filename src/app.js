13de02a7080b4825b834cd057dbeff2aa9f82fed
Shyam Chen <shyamchen1994@gmail.com>
2016-12-22 10:00:22 +0800

    320th Commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
+import { Observable } from '@reactivex/rxjs/dist/es6/Observable';
+import '@reactivex/rxjs/dist/es6/add/observable/of';
+
+import './app-routing';
 import './styles/global.css';
 
 import { load } from './functions';
 import firebaseConfig from './assets/datas/firebase.config.json';
-
-import { HOME_EN, HOME_ZH } from './pages/home';
-import { ABOUT_EN, ABOUT_ZH } from './pages/about';
-import { CONTACT_EN, CONTACT_ZH } from './pages/contact';
-import { ERROR } from './pages/error';
-
-import { Observable } from '@reactivex/rxjs/dist/es6/Observable';
-import '@reactivex/rxjs/dist/es6/add/observable/of';
-
-Observable.of(1,2,3);
 
 /**
  * @name Initialization
@@ -27,9 +21,9 @@ Promise.all([
 
 firebase.initializeApp(firebaseConfig);
 
-/*
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
+  navigator.serviceWorker
+    .register('service-worker.js')
     .then((registration) => {
       registration.onupdatefound = () => {
         if (navigator.serviceWorker.controller) {
@@ -42,34 +36,11 @@ if ('serviceWorker' in navigator) {
                 break;
               case 'redundant':
                 throw new Error('...');
-              default:
             }
           };
         }
       };
     });
 }
-*/
 
-/**
- * @deprecated
- */
-switch (true) {
-  case /zh/.test(navigator.language):
-    page('/', HOME_ZH);
-    break;
-  default:
-    page('/', HOME_EN);
-}
-
-page('/en/home', HOME_EN);
-page('/en/about', ABOUT_EN);
-page('/en/contact', CONTACT_EN);
-
-page('/zh/home', HOME_ZH);
-page('/zh/about', ABOUT_ZH);
-page('/zh/contact', CONTACT_ZH);
-
-page('*', ERROR);
-
-page();
+Observable.of(1,2,3);
