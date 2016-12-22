04bf86e15c9d743a32f20f8ca40161d07b54cde9
Shyam Chen <shyamchen1994@gmail.com>
2016-12-22 18:22:25 +0800

    323rd Commit

--- src/vendor.js
+++ src/vendor.js
@@ -1,3 +1,4 @@
+import '@material/button/dist/mdc.button.css';
 import 'material-design-lite/dist/material.indigo-pink.min.css';
 import 'material-design-lite/material';
 import 'firebase/firebase';
