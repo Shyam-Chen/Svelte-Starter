5e29edc6af878bcd68aadd258678345f20fee3f9
Shyam Chen <shyamchen1994@gmail.com>
2016-12-29 12:32:22 +0800

    346th Commit

--- tools/tasks/unit.js
+++ tools/tasks/unit.js
@@ -1,11 +1,11 @@
 import { join } from 'path';
 import gulp from 'gulp';
-import util from 'gulp-util';
-import Karma from 'karma';
+import { env } from 'gulp-util';
+import { Server } from 'karma';
 
 gulp.task('unit', (done) => {
-  new Karma.Server({
+  new Server({
     configFile: join(__dirname, '../config', 'karma.conf.js'),
-    singleRun: util.env.type === 'watch' ? false : true
+    singleRun: env.type === 'watch' ? false : true
   }, done).start();
 });
