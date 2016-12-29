6c302a21ab0d476123878e310c988ff2f6193e78
Shyam Chen <shyamchen1994@gmail.com>
2016-12-29 15:40:49 +0800

    349th Commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,14 @@
 import gulp from 'gulp';
+import { env } from 'gulp-util';
 import runsequence from 'run-sequence';
 import requiredir from 'require-dir';
 
 requiredir('./tools/tasks');
 
-gulp.task('dev', (done) => runsequence('build', 'watch', 'serve', done));
-gulp.task('dev-watch', (done) => runsequence('build', 'watch', done));
+gulp.task('dev', (done) => {
+  if (env.serve) {
+    return runsequence('build', 'watch', done);
+  } else {
+    return runsequence('build', 'watch', 'serve', done);
+  }
+});
