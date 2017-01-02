e9392db67f120c67d72db3ee684cf16b2c5cc405
Shyam Chen <shyamchen1994@gmail.com>
2017-01-02 21:20:02 +0800

    381st Commit

--- tools/constants.js
+++ tools/constants.js
@@ -3,4 +3,12 @@ import { join } from 'path';
 export const SOURCE_ROOT = join(__dirname, '..', 'src');
 export const DIST_ROOT = join(__dirname, '..', 'public');
 
+export const PAGES_ROOT = join(SOURCE_ROOT, 'pages');
+
+export const COMPONENTS_ROOT = join(SOURCE_ROOT, 'components');
+
+export const SCRIPTS_ROOT = join(SOURCE_ROOT, 'scripts');
+export const STYLES_ROOT = join(SOURCE_ROOT, 'styles');
+export const TEMPLATES_ROOT = join(SOURCE_ROOT, 'templates');
+
 export const ASSETS_ROOT = join(SOURCE_ROOT, 'assets');
