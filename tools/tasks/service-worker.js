9056b0412ba0e5145f991cd7ee0b25ca25bcdcab
Shyam Chen <shyamchen1994@gmail.com>
2017-01-02 21:12:41 +0800

    379th Commit

--- tools/tasks/service-worker.js
+++ tools/tasks/service-worker.js
@@ -1,56 +1,11 @@
 import { join } from 'path';
 import gulp from 'gulp';
+import { log } from 'gulp-util';
 import swprecache from 'sw-precache';
+import runsequence from 'run-sequence';
 
 import pkg from '../../package.json';
 import { DIST_ROOT } from '../constants';
-
-/*function writeServiceWorkerFile(rootDir, handleFetch, callback) {
-  var config = {
-    cacheId: packageJson.name,
-
-    // dynamicUrlToDependencies: {
-    //   'dynamic/page1': [
-    //     path.join(rootDir, 'views', 'layout.jade'),
-    //     path.join(rootDir, 'views', 'page1.jade')
-    //   ],
-    //   'dynamic/page2': [
-    //     path.join(rootDir, 'views', 'layout.jade'),
-    //     path.join(rootDir, 'views', 'page2.jade')
-    //   ]
-    // },
-
-    // If handleFetch is false (i.e. because this is called from generate-service-worker-dev), then
-    // the service worker will precache resources but won't actually serve them.
-    // This allows you to test precaching behavior without worry about the cache preventing your
-    // local changes from being picked up during the development cycle.
-    handleFetch: handleFetch,
-    logger: $.util.log,
-    runtimeCaching: [{
-      // See https://github.com/GoogleChrome/sw-toolbox#methods
-      urlPattern: /runtime-caching/,
-      handler: 'cacheFirst',
-      // See https://github.com/GoogleChrome/sw-toolbox#options
-      options: {
-        cache: {
-          maxEntries: 1,
-          name: 'runtime-cache'
-        }
-      }
-    }],
-    staticFileGlobs: [
-      rootDir + '/css/**.css',
-      rootDir + '/**.html',
-      rootDir + '/images/**.*',
-      rootDir + '/js/**.js'
-    ],
-    stripPrefix: rootDir + '/',
-    // verbose defaults to false, but for the purposes of this demo, log more.
-    verbose: true
-  };
-
-  swPrecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
-}*/
 
 gulp.task('generate-service-worker', () => {
   return swprecache.write(join(DIST_ROOT, 'service-worker.js'), {
@@ -67,28 +22,21 @@ gulp.task('generate-service-worker', () => {
   });
 });
 
-
-import packageJson from '../../package.json';
-const path = require('path');
-import runsequence from 'run-sequence';
-
-import { log } from 'gulp-util';
-
 const DEV_DIR = 'app';
 const DIST_DIR = 'dist';
 
 function writeServiceWorkerFile(rootDir, handleFetch, callback) {
   const config = {
-    cacheId: packageJson.name,
+    cacheId: pkg.name,
     /*
     dynamicUrlToDependencies: {
       'dynamic/page1': [
-        path.join(rootDir, 'views', 'layout.jade'),
-        path.join(rootDir, 'views', 'page1.jade')
+        join(rootDir, 'views', 'layout.jade'),
+        join(rootDir, 'views', 'page1.jade')
       ],
       'dynamic/page2': [
-        path.join(rootDir, 'views', 'layout.jade'),
-        path.join(rootDir, 'views', 'page2.jade')
+        join(rootDir, 'views', 'layout.jade'),
+        join(rootDir, 'views', 'page2.jade')
       ]
     },
     */
@@ -121,7 +69,7 @@ function writeServiceWorkerFile(rootDir, handleFetch, callback) {
     verbose: true
   };
 
-  swprecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
+  swprecache.write(join(rootDir, 'service-worker.js'), config, callback);
 }
 
 gulp.task('build-service-worker', function(callback) {
