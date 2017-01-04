2da1105e13f34c4be3cc239e386e1965e158cdd9
Shyam Chen <shyamchen1994@gmail.com>
2017-01-04 13:56:33 +0800

    392nd Commit

--- src/scripts/redux-observable/index.js
+++
@@ -1,4 +0,0 @@
-export { createEpicMiddleware } from './create-epic-middleware';
-export { ActionsObservable } from './actions-observable';
-export { combineEpics } from './combine-epics';
-export { EPIC_END } from './constants';
