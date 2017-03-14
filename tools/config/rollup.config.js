b4d67a3810d45f6bab007ec4725ac29cfa462e5b
Shyam Chen <shyamchen1994@gmail.com>
2017-03-14 09:31:13 +0800

    523rd Commit

--- tools/config/rollup.config.js
+++ tools/config/rollup.config.js
@@ -57,7 +57,30 @@ const plugins = () => {
     babel({
       babelrc: false,
       presets: [['latest', { es2015: { modules: false } }]],
-      plugins: ['external-helpers', 'transform-function-bind', 'lodash'],
+      plugins: ['external-helpers', 'transform-function-bind', [
+        'transform-imports', {
+          'lodash': {
+            transform: 'lodash/${member}',
+            preventFullImport: true
+          },
+          'rxjs': {
+            transform: 'rxjs/${member}',
+            preventFullImport: true
+          },
+          'rxjs/observable': {
+            transform: 'rxjs/observable/${member}',
+            preventFullImport: true
+          },
+          'rxjs/operator': {
+            transform: 'rxjs/operator/${member}',
+            preventFullImport: true
+          },
+          'rxjs/scheduler': {
+            transform: 'rxjs/scheduler/${member}',
+            preventFullImport: true
+          }
+        }
+      ]],
       exclude: 'node_modules/**'
     }),
     globals(),
