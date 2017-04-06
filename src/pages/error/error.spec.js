bd49f107587d6e098542a44c2119b3fada85756a
Shyam Chen <shyamchen1994@gmail.com>
2017-04-06 11:10:38 +0800

    582nd Commit

--- src/pages/error/error.spec.js
+++ src/pages/error/error.spec.js
@@ -7,6 +7,10 @@ describe('Error', () => {
     expect(error).toBeDefined();
   });
 
+  it('toBe', () => {
+    expect(typeof error).toBe('function');
+  });
+
   it('test', () => {
     const dummyElement = document.createElement('div');
     document.querySelector = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
--- src/pages/home/home.spec.js
+++ src/pages/home/home.spec.js
@@ -1,7 +1,23 @@
 import { home } from './home';
 
+import { layout } from '../../components/layout';
+
+import template from './home.html';
+import data from './home.json';
+
 describe('Home', () => {
   it('toBeDefined', () => {
     expect(home).toBeDefined();
+  });
+
+  it('toBe', () => {
+    expect(typeof home).toBe('function');
+  });
+
+  it('test', () => {
+    page('/', () => {
+      layout('home', template(data));
+      componentHandler.upgradeAllRegistered();
+    });
   });
 });
