95eb4b8584d7ff0eab5f81214f78ba2a2b3ac87c
Shyam Chen <shyamchen1994@gmail.com>
2017-04-16 14:24:37 +0800

    614th Commit

--- src/vendor.js
+++ src/vendor.js
@@ -10,13 +10,18 @@
 // import '@material/card/dist/mdc.card.css';
 // import '@material/checkbox/dist/mdc.checkbox.css';
 // import '@material/checkbox/dist/mdc.checkbox';
+// import '@material/dialog/dist/mdc.dialog.css';
+// import '@material/dialog/dist/mdc.dialog';
 // import '@material/drawer/dist/mdc.drawer.css';
 // import '@material/drawer/dist/mdc.drawer';
 // import '@material/elevation/dist/mdc.elevation.css';
 // import '@material/fab/dist/mdc.fab.css';
 // import '@material/form-field/dist/mdc.form-field.css';
+// import '@material/grid-list/dist/mdc.grid-list.css';
+// import '@material/grid-list/dist/mdc.grid-list';
 // import '@material/icon-toggle/dist/mdc.icon-toggle.css';
 // import '@material/icon-toggle/dist/mdc.iconToggle';
+import '@material/layout-grid/dist/mdc.layout-grid.css';
 // import '@material/list/dist/mdc.list.css';
 // import '@material/menu/dist/mdc.menu.css';
 // import '@material/menu/dist/mdc.menu';
@@ -28,9 +33,11 @@
 // import '@material/select/dist/mdc.select';
 // import '@material/snackbar/dist/mdc.snackbar.css';
 // import '@material/snackbar/dist/mdc.snackbar';
+import '@material/switch/dist/mdc.switch.css';
 // import '@material/textfield/dist/mdc.textfield.css';
 // import '@material/textfield/dist/mdc.textfield';
 // import '@material/theme/dist/mdc.theme.css';
+import '@material/toolbar/dist/mdc.toolbar.css';
 // import '@material/typography/dist/mdc.typography.css';
 import 'material-design-lite/dist/material.indigo-pink.min.css';  // @deprecated
 import 'material-design-lite/material';  // @deprecated
