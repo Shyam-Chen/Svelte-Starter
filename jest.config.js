62d677c706437770bcb9176d255ade41e86a3499
Shyam-Chen <shyamchen1994@gmail.com>
2018-01-24 14:38:55 +0800

    984th Commit

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,13 @@
 module.exports = {
+  moduleNameMapper: {
+    '~(.*)': '<rootDir>/src$1',
+    '^.*[.](jpg|JPG|gif|GIF|png|PNG|less|LESS|css|CSS|html|pug)$': '<rootDir>/tools/empty.js'
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleFileExtensions: ['js', 'json'],
+  setupFiles: [
+    '<rootDir>/tools/setup.js'
+  ]
 };
---
+++ tools/setup.js
@@ -0,0 +1,3 @@
+import page from 'page';
+
+global.page = page;
