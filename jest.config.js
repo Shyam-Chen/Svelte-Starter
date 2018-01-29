497e03efe40aad78fca2afb8d22d02660a31b46f
Shyam-Chen <shyamchen1994@gmail.com>
2018-01-29 15:00:10 +0800

    996th Commit

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,13 @@ module.exports = {
     '~(.*)': '<rootDir>/src$1',
     '^.*[.](jpg|JPG|gif|GIF|png|PNG|less|LESS|css|CSS|html|pug)$': '<rootDir>/tools/empty.js'
   },
+  setupFiles: [
+    '<rootDir>/tools/setup.js'
+  ],
   transform: {
     '^.+\\.js$': 'babel-jest'
   },
-  moduleFileExtensions: ['js', 'json'],
-  setupFiles: [
-    '<rootDir>/tools/setup.js'
+  transformIgnorePatterns: [
+    'node_modules/(?!@material)/'
   ]
 };
