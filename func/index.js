1c8c291978f986bcd2c1a4c25db4c178b24911c7
Shyam-Chen <shyamchen1994@gmail.com>
2018-02-21 16:42:28 +0800

    1006th Commit

--- func/index.js
+++ func/index.js
@@ -1,18 +1,36 @@
 import functions from 'firebase-functions';
 import admin from 'firebase-admin';
+// import express from 'express';
+// import cors from 'cors';
 
 admin.initializeApp(functions.config().firebase);
+
+const cors = require('cors')({ origin: true });
+
+// const foo = express();
+// const bar = express();
+
+// foo.get('/', ...);
+// bar.get('/', ...);
 
 /**
  * @example POST /addText?text=${text}
  */
 export const addText = functions.https.onRequest((req, res) => {
-  const { text } = req.query;
+  cors(req, res, () => {
+    const { text } = req.query;
 
-  admin.database()
-    .ref('/messages')
-    .push({ text })
-    .then(() => {
-      res.status(200).json({ message: 'Text saved.' });
-    });
+    admin.database()
+      .ref('/messages')
+      .push({ text })
+      .then(() => {
+        res.status(200).json({ message: 'Text saved.' });
+      });
+  });
 });
+
+// /foo
+// export const foo = functions.https.onRequest(foo);
+
+// /bar
+// export const bar = functions.https.onRequest(bar);
