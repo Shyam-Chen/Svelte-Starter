0e767388d86b13d79c9717e41aaf6a843966aac5
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-01 11:13:13 +0800

    1015th Commit

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   moduleNameMapper: {
     '~(.*)': '<rootDir>/src$1',
-    '^.*[.](jpg|JPG|gif|GIF|png|PNG|less|LESS|css|CSS|html|pug)$': '<rootDir>/tools/utils/empty-mapper.js'
+    '^.*[.](jpg|JPG|gif|GIF|png|PNG|less|LESS|css|CSS|html|pug)$': '<rootDir>/tools/utils/empty-mapper.js',
   },
   setupFiles: [
-    '<rootDir>/tools/utils/setup-files.js'
+    '<rootDir>/tools/utils/setup-files.js',
   ],
   transform: {
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.js$': 'babel-jest',
   },
   // transformIgnorePatterns: [
   //   'node_modules/(?!@material)/'
