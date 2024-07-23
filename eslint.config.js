400c6d122ce8bd9f80b548823dbc0ef2566b44dd
Shyam-Chen <shyamchen1994@gmail.com>
2024-07-23 10:26:55 +0800

    1,088th Commit

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['build/', '.svelte-kit/', 'dist/'],
+  },
+
+  js.configs.recommended,
+
+  ...ts.configs.recommended,
+
+  ...svelte.configs['flat/recommended'],
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+
+  prettier,
+  ...svelte.configs['flat/prettier'],
+];
